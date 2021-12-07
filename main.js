6eb64389a6ef129f078614e696357dc45d11b101
Jessica <koda@horizons.gg>
2021-12-07 23:40:45 +1100

    Rollback

--- main.js
+++ main.js
@@ -11,19 +11,11 @@ bouncy(function(req, bounce) {
 
     //? Software
     if (req.headers.host === 'grafana.horizons.gg') return bounce(3000)
-    if (req.headers.host === 'eco.horizons.gg') {
-        if (!req.url.includes('authtoken') || req.url.includes('passthrough=true')) return bounce(3001)
-        return bounce(9080)
-    }
+    if (req.headers.host === 'eco.horizons.gg') return bounce(3001)
     if (req.headers.host === 'se.horizons.gg') return bounce(8090)
 
 
     //? Development
     if (req.headers.host === 'dev.horizons.gg') return bounce(8080)
-
-    if (req.headers.host === 'ecodev.horizons.gg') {
-        if (!req.url.includes('authtoken') || req.url.includes('passthrough=true')) return bounce(3001)
-        return bounce(8080)
-    }
 
 }).listen(80)
