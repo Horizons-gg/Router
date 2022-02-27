88d58026391869187cf3e449e1f6b96e4cba8118
Jessica <koda@horizons.gg>
2022-02-27 11:21:26 +1100

    added 2mef

--- main.js
+++ main.js
@@ -2,12 +2,14 @@ const bouncy = require('bouncy')
 
 bouncy(function(req, bounce) {
 
-    if (req.headers.referer) console.log(`${req.method}: ${req.headers.referer}`)
+    if (req.headers.referer) console.log(`${req.method}: ${req.headers.referer} - ${req.headers['x-forwarded-for'] || '*.*.*.*'}`)
 
     //? Live
     if (req.headers.host === 'www.horizons.gg') return bounce(9080)
     if (req.headers.host === 'api.horizons.gg') return bounce(9081)
     if (req.headers.host === 'discord.horizons.gg') return bounce(9082)
+    if (req.headers.host === '2mef.net') return bounce(9083)
+    if (req.headers.host === 'www.2mef.net') return bounce(9083)
 
     //? Software
     if (req.headers.host === 'grafana.horizons.gg') return bounce(3000)
@@ -19,3 +21,7 @@ bouncy(function(req, bounce) {
     if (req.headers.host === 'dev.horizons.gg') return bounce(8080)
 
 }).listen(80)
+
+
+
+setTimeout(() => { process.exit(0) }, 1000 * 60 * 60)
